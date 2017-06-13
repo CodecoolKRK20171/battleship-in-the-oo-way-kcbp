e323eaa0c25c90a51c35b40d873a9d80f4b4a5f2
Pawel Rybka <pwl.rybka@gmail.com>
2017-06-13 14:52:18 +0200

    new version of ship.py and ocean.py

---
+++ ocean.py
@@ -0,0 +1,64 @@
+from ship import Ship
+
+class Ocean:
+
+    def __init__(self):
+        size_of_board = 10
+        self.ships = []
+        self.board = []
+        for i in range(size_of_board):
+            self.board.append(['~']*size_of_board)
+
+
+    def __str__(self):
+        output = ''
+        for row in self.board:
+            output += ''.join(row)+'\n'
+        return output
+
+    # def add_ship(self, ship):
+    #    self.ships.append(ship)
+        
+
+    def insert_shoot(self, cords):
+        x = 0
+        y = 1
+        column = cords[x]
+        row = cords[y]
+        # print('{} {}'.format(row, column))
+        for ship in self.ships:
+            for position in ship.positions:
+                if (column, row) == position:
+                    self.board[row-1][column-1] = 'X'
+                    return True
+        self.board[row-1][column-1] = 'O'
+        return False
+
+    def insert_ships(self):
+        for ship in self.ships:
+            for position in ship.positions:
+                column, row = position
+                self.board[row-1][column-1] = 'X'
+
+def main():
+    ship1 = Ship('Cruiser', (1,2), True)
+    ship2 = Ship('Battleship', (5,5))
+
+    player_ocean = Ocean()
+    enemy_ocean = Ocean()
+    player_ocean.ships.append(ship1)
+    player_ocean.ships.append(ship2)
+    enemy_ocean.ships.append(ship1)
+    enemy_ocean.ships.append(ship2)
+    enemy_ocean.insert_shoot((2,2))
+    enemy_ocean.insert_shoot((1,2))
+    enemy_ocean.insert_shoot((1,3))
+    # enemy_ocean = player_ocean.copy.deepcopy()
+    player_ocean.insert_ships()
+    print(player_ocean)
+    print()
+    print(enemy_ocean)
+
+
+if __name__ == '__main__':
+    main()
