0bed2c29b72f427171eef005b2764cb1a36eea5f
Pawel Rybka <pwl.rybka@gmail.com>
2017-06-14 13:44:40 +0200

    Merge main.py

--- main.py
+++ main.py
@@ -33,9 +33,28 @@ def main():
     player_one = set_player('First')
     player_two = set_player('Second')
 
-    print(player_one.player_ocean)
+    player_one.enemy_ocean.ships = player_two.player_ocean.ships
+    player_two.enemy_ocean.ships = player_one.player_ocean.ships
 
+    # current_player = player_one
+    print(player_one.player_ocean + '\n')
+    print(player_one.enemy_ocean)
+    cords = player_one.choose_shot_cords()
+    player_two.enemy_ocean.insert_shoot(cords)
 
+    print(player_one.player_ocean + '\n')
+    print(player_one.enemy_ocean)
+    # counter_player_one = 0
+    # counter_player_two = 0
+    # total_ships_squares = 17
+# 
+    # while counter_player_one != total_ships_squares and 
+        #   counter_player_two != total_ships_squares:
+# 
+        # print(current_player.player_ocean + '\n')
+        # print(current_player.enemy_ocean)
+        # cords = current_player.choose_shot_cords()
+        # player_two.enemy_ocean.insert_shoot(cords)   
 
 if __name__ == '__main__':
     main()
