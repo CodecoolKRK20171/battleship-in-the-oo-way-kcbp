60c87b5be12b474c0c2f3d3db649f7e47231bc10
CyprianS <szwajka.cyprian@gmail.com>
2017-06-14 10:48:18 +0200

    refactoring. 'choose_shot_cords' and 'choose_initial_ship_position' functionalities close in separate method 'take_coordinates'

--- player.py
+++ player.py
@@ -3,44 +3,24 @@ import re
 
 
 class Player:
-    pass
 
     def __init__(self, name):
         self.name = name
         self.player_ocean = Ocean()
         self.enemy_ocean = Ocean()
 
-    def choose_initial_ships_position(self, ship_name):
-        """
-        Takes ships positions from user.
-        returns
-        ---------
-        dictionary with ships names as keys and tupples of given_position
-        as values
-        """
-        new_ship_data = []
-        letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-        numbers_str = [str(i) for i in list(range(1, 11))]
+    def choose_initial_ship_position(self, ship_name):
+        ship_position_cords = Player.take_coordinates('Enter ship starting coordinates for', ship_name)
+        new_ship_data = [ship_name, ship_position_cords]
 
-        ship_cords = input('Enter ship starting coordinates for ' + ship_name + ': (f.e. a6/A6)').upper()
-        if not re.match("^[a-jA-J][1-9]$|^[a-jA-J][1][0]$", ship_cords):
-            raise ValueError('Wrong Input!')
-
-        else:
-            ship_cords_list = [ship_cords[0], ship_cords[1]]
-            for i in range(len(letters)):
-                if ship_cords_list[0] == letters[i]:
-                    ship_cords_list[0] = numbers_str[i]
-            new_ship_data = [ship_name, (int(ship_cords_list[0]), int(ship_cords[1]))]
-
-        self.is_vertical(ship_name)
+        self.is_vertical(ship_name, new_ship_data)
 
         return new_ship_data
 
     def is_vertical(self, ship_name, new_ship_data):
         """indicates ship direction. True if vertical, False if horizontal"""
 
-        is_vertical = input('If situation of ' + ship_name + 'suppouse to be vertical, input 1, otherwise 0: ')
+        is_vertical = input('If situation of ' + ship_name + ' suppouse to be vertical, input 1, otherwise 0: ')
         if is_vertical == "1":
             new_ship_data.append(True)
         elif is_vertical == "0":
@@ -56,23 +36,27 @@ class Player:
         -------
         tuple with two integers
         """
+        shot_cords = Player.take_coordinates('Enter shot coordinates')
 
-        letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-        numbers_str = [str(i) for i in list(range(1, 11))]
-
-        shot_cords = input('Enter shoot coordinates: (f.e. a6/A6)').upper()
-
-        if not re.match("^[a-jA-J][1-9]$|^[a-jA-J][1][0]$", shot_cords):
-            raise ValueError('Wrong Input!')
-
-        else:
-            shot_cords_list = [shot_cords[0], shot_cords[1]]
-            for i in range(len(letters)):
-                if shot_cords_list[0] == letters[i]:
-                    shot_cords_list[0] = numbers_str[i]
-            shot_cords_tuple = (int(shot_cords_list[0]), int(shot_cords[1]))
-
-        return shot_cords_tuple
+        return shot_cords
 
     def is_win(self):
         pass
+
+    @staticmethod
+    def take_coordinates(input_message, ship_name=""):
+        letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        numbers_str = [str(i) for i in list(range(1, 11))]
+
+        ship_or_shot_cords = input('{} {} (f.e. a6/A6):'.format(input_message, ship_name)).upper()
+
+        if re.match("^[a-jA-J][1-9]$|^[a-jA-J][1][0]$", ship_or_shot_cords):
+            cords_list = [ship_or_shot_cords[0], ship_or_shot_cords[1:]]
+            for i in range(len(letters)):
+                if cords_list[0] == letters[i]:
+                    cords_list[0] = numbers_str[i]
+            cords_tuple = (int(cords_list[0]), int(ship_or_shot_cords[1:]))
+        else:
+            raise ValueError('Wrong Input!')
+
+        return cords_tuple
